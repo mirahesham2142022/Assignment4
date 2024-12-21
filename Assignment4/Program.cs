d1003eb7ed5b4aa41b9842e678440befd50a7f11
mirahesham2142022 <mia.hesham2003@gmail.com>
2024-12-21 18:52:15 +0200

    Assignment4

---
+++ Assignment4/Program.cs
@@ -0,0 +1,158 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For loop
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region for _ for_each(works on a copy of array)
+
+
+
+            // int[] nUmbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            ///use for if u want to acesss index
+            //for (int i = 0; i < nUmbers.Length; i++)
+            //{
+            //if(nUmbers[i]=5)
+            //break;
+            //    Console.WriteLine(nUmbers[i]);
+            //} 
+            //class implemnt inteface Ienuemrble,Cant edit any value inside array (ust for display)
+            //foreach(int number in nUmbers)
+            //{
+            //    Console.WriteLine(number);
+            //   // Console.WriteLine(number+10); applicable bec I dont edit r change the values of array 
+            //}    
+            #endregion
+            #region while-Do_while
+            //int num;
+            //bool flag;
+            //do
+            //{
+            //    Console.WriteLine("Enter even");
+            //    // num = int.Parse(Console.ReadLine());
+            //    flag = int.TryParse(Console.ReadLine(), out num);
+            //} while (num % 2 == 1 || !flag);
+            //Console.WriteLine($"{ num } is Even");
+
+            //while ((num % 2 == 1 || !flag)
+            //{
+            //    Console.WriteLine("Enter even");
+            //    // num = int.Parse(Console.ReadLine());
+            //    flag = int.TryParse(Console.ReadLine(), out num);
+            //}
+            //Console.WriteLine($"{num}is Even");
+            #endregion
+
+
+            #region string
+            //Immutable
+            //Class->RefreceType
+            //String Name1, Name2;
+            //Name1 = ("Ahmed");
+            //Name2 = ("Ahmed");
+            //Console.WriteLine(Name1.GetHashCode());
+            //Console.WriteLine(Name2.GetHashCode());
+
+            //Name1 = ("Mira");
+            //Name2 = ("Dina");
+            //Console.WriteLine(Name1.GetHashCode());
+            //Console.WriteLine(Name2.GetHashCode());
+            //Name2 = Name1;
+            //Console.WriteLine(Name1.GetHashCode());
+            //Console.WriteLine(Name2.GetHashCode());
+            #endregion
+            #region   String builder 
+
+            //linkedlist of chars
+            //StringBuilder Message;
+            //Message= new StringBuilder("Mira");
+            //Console.WriteLine(Message);
+            //Console.WriteLine(Message.GetHashCode());
+            //Message.Append("Hesham");
+            //Console.WriteLine(Message.GetHashCode());
+
+            #endregion
+            #region StringBuilder Funcions 
+            //StringBuilder Message;
+            //Message = new StringBuilder("Mira");
+            //Message.Remove(0, 3);
+            //Message.Insert(0,"Hello");
+            //Message.Append(" Hesham ");
+            //Console.WriteLine(Message.ToString());
+
+            #endregion
+
+            #region Array1D 
+            //Referncetype
+            //int[] Nums = { 1, 2, 3 };
+            //int[] Nums = new int[] { 1, 2, 3 };
+            //int[]Nums = new int[] { 1, 2, 3 };
+            //int[] Nums = new int[3];
+            // for (int i = 0; i<Nums.Length; i++) {
+            //    Console.WriteLine($"Enter elemnt{i+1}");
+            //    Nums[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < Nums.Length; i++)
+            //{
+            //    Console.WriteLine(Nums[i]);
+            //}
+            //Console.WriteLine("----------------------------------------");
+            //Console.WriteLine(Nums.Length);
+            //Console.WriteLine(Nums.Rank);
+
+
+            #endregion
+
+            #region Array2D
+            //int[,] Marks = new int[3, 4];
+            //for (int i = 0; i < Marks.GetLength(0); i++)
+            //{
+            //    bool flag;
+            //    Console.WriteLine($"enter Grades Student {i+1}");
+            //    for (int k = 0; k< Marks.GetLength(1); k++)
+            //    {
+            //        Console.WriteLine($"enter Grades of subjects {k + 1}");
+            //       flag= int.TryParse(Console.ReadLine(), out Marks[i, k]);
+            //        if (flag && Marks[i, k] >= 0)
+            //        {
+            //            k++;
+            //        }
+
+            //    }
+            //}
+            #endregion
+
+            #region iterate On 2d Array on  one loop
+            //int[,] Marks = new int[3, 5];
+            //for (int i = 0; i < Marks.GetLength(0); i++)
+            //{
+            //    Console.WriteLine(Marks[i / Marks.GetLength(1), i % Marks.GetLength(1)]);
+            //}
+            #endregion
+
+            #region JudgedArray
+            int[][] Number = new int[3][];
+            Number[0] = new int[3] { 1, 2, 3 };
+            Number[1] = new int[2] { 1, 2 };
+            Number[2] = new int[3];
+            for (int i = 0; i < Number.Length; i++)
+            {
+                for (int j = 0; j < Number[i].Length;j++)
+                {
+                    Console.WriteLine(Number[i][j]);
+                }
+                Console.WriteLine("-----------");
+            }
+            #endregion
+        }
+    }
+}
---
+++ Assignment4_Questions/Program.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Assignment4_Questions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+            //Console.Write("Enter an integer: ");
+            //int x = int.Parse(Console.ReadLine());
+
+
+            //for (int i = 1; i <= x; i++)
+            //{
+            //    Console.Write(i);
+            //    Console.WriteLine(" ");
+
+            //}
+            #endregion
+            #region 2- Write a program that allows the user to insert an integer then print a multiplication table up to 12. 
+            //Console.Write("Enter an integer: ");
+            //int x = int.Parse(Console.ReadLine());
+
+
+            //for (int i = 1; i <= x; i++)
+            //{
+            //    Console.Write(i*12);
+            //    Console.WriteLine(" ");
+
+            //}
+            #endregion
+
+            #region 3 - Write a program that allows to user to insert number then print all even numbers between 1 to this number
+            //Console.Write("Enter an integer: ");
+            //int x = int.Parse(Console.ReadLine());
+
+
+            //for (int i = 1; i <= x; i++)
+            //{
+            //    if(i%2==0)
+            //    Console.Write(i);
+            //    Console.WriteLine(" ");
+
+            //}
+
+            #endregion
+
+            #region  Write a program that takes two integers then prints the power.
+            //Console.Write("Enter num1: ");
+            //int num1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter num2: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+
+            //int result = 1;
+            //for (int i = 0; i < num2; i++)
+            //{
+            //    result *= num1;
+            //}
+            //Console.WriteLine($"{num1}^{num2} = {result}");
+
+            #endregion
+            #region Q5 ReverseSring
+
+            //    Console.Write("Enter string: ");
+            //    string word = Console.ReadLine();
+            //    string reversedString = "";
+            //    for (int i = word.Length - 1; i >= 0; i--)
+            //    {
+            //        reversedString += word[i];
+            //    }
+            //    Console.WriteLine("Reversed string: " + reversedString);
+            //} 
+            #endregion
+
+            Console.Write("Enter the size of the identity  ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+            
+                Console.WriteLine();
+            }
+        }
+        #region q12 Write a program in C# Sharp to find the sum of all elements of the array. 
+        //int[] numbers = { 5, 10, 15, 20, 25 };
+        //int sum = 0;
+        //for (int i = 0; i < numbers.Length; i++)
+        //{
+        //    sum += numbers[i];
+        //}
+        //Console.WriteLine("The sum  " + sum);
+        #endregion
+
+        #region Q15 Print max and min
+        //int[] array = { 3, 5, 7, 2, 8, 1, 9, 4 };
+        // int max = array[0];
+        // int min = array[0];
+        // for (int i = 1; i < array.Length; i++)
+        // {
+        //     if (array[i] > max)
+        //     {
+        //         max = array[i];
+        //     }
+
+        //     if (array[i] < min)
+        //     {
+        //         min = array[i];
+        //     }
+        // }
+        // Console.WriteLine("Maximum " + max);
+        // Console.WriteLine("Minimum " + min); 
+        #endregion
+        #region Q19 Reverse order
+        //int[] array = { 1, 2, 3, 4, 5 };
+        //for (int i = array.Length-1 ; i >= 0; i--)
+        //{
+        //    Console.Write(array[i] + " ");
+        //}
+
+        #endregion
+
+    }
+    }
